d687c6eed3e97d1f6bc09ee7b260ed778fe061af
Alexandros Georgiou <106652543+gumidnight@users.noreply.github.com>
2025-07-26 11:42:52 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+
+---
+
+### `main.tf`
+
+```hcl
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  vpc_security_group_ids = [var.security_group_id]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
